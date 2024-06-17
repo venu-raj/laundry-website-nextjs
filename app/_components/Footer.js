e91e20015af972f4334e62794ec51e74362e4c5e
venu-raj <venudev2580@gmail.com>
2024-06-17 18:03:37 +0530

    errors fixed on ui

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -4,7 +4,6 @@ export default function Footer() {
       <footer className="bg-white dark:bg-gray-900">
         <div className="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
           <a href="#">
-            {/* <img className="w-auto h-7" src="https://merakiui.com/images/full-logo.svg" alt=""> */}
           </a>
 
           <p className="text-sm text-gray-600 dark:text-gray-300">© Copyright 2021. All Rights Reserved.</p>
